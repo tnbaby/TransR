cd38b65a2ff4b964f8520f74b8a3b31a650f4a24
tnbaby <tnbaby@sjtu.edu.cn>
2017-11-28 11:09:46 +0800

    multi_thread

--- Trans_PY/TransN/multi_Train_TransE.cpp
+++ Trans_PY/TransN/multi_Train_TransE.cpp
@@ -7,8 +7,9 @@
 #include<ctime>
 #include<cmath>
 #include<cstdlib>
+#include<pthread.h>
 using namespace std;
-
+#define THREADS_NUM 16
 
 #define pi 3.1415926535897932384626433832795
 
@@ -54,13 +55,31 @@ char buf[100000],buf1[100000];
 int relation_num,entity_num;
 map<string,int> relation2id,entity2id;
 map<int,string> id2entity,id2relation;
-
+double res_thread[THREADS_NUM];
 
 map<int,map<int,int> > left_entity,right_entity;
 map<int,double> left_num,right_num;
-class Train{
 
-public:
+int n,method;
+double res;//loss function value
+double count,count1;//loss function gradient
+double rate,margin, con_alpha;
+double belta;
+int batchsize;
+int nbatches;
+int nepoch;
+vector<int> fb_h,fb_l,fb_r;
+vector<vector<int> > feature;
+vector<vector<double> > relation_vec,entity_vec;
+vector<vector<double> > relation_tmp,entity_tmp;
+
+double norm(vector<double> &a);
+void bfgs();
+void *rand_sel(void*);
+void train_kb(int, int, int, int, int, int, int);
+//class Train{
+
+//public:
 	map<pair<int, int>, vector<int> >entity_neighbors;
 	map<pair<int,int>, map<int,int> > ok;
     void add(int x,int y,int z)
@@ -109,16 +128,8 @@ public:
         bfgs();
     }
 
-private:
-    int n,method;
-    double res;//loss function value
-    double count,count1;//loss function gradient
-    double rate,margin, con_alpha;
-    double belta;
-    vector<int> fb_h,fb_l,fb_r;
-    vector<vector<int> > feature;
-    vector<vector<double> > relation_vec,entity_vec;
-    vector<vector<double> > relation_tmp,entity_tmp;
+//private:
+
     double norm(vector<double> &a)
     {
         double x = vec_len(a);
@@ -135,96 +146,89 @@ private:
         return res;
     }
 
+    void *rand_sel(void *tid_void)
+    {
+	long tid = (long) tid_void;
+	for (int k=0; k<batchsize; k++)
+	{
+		int i=rand_max(fb_h.size());
+		int j=rand_max(entity_num);
+		double pr = 1000*right_num[fb_r[i]]/(right_num[fb_r[i]]+left_num[fb_r[i]]);
+		if (method ==0)
+			pr = 500;
+		if (rand()%1000<pr)
+		{
+			while (ok[make_pair(fb_h[i],fb_r[i])].count(j)>0)
+				j=rand_max(entity_num);
+			train_kb(fb_h[i],fb_l[i],fb_r[i],fb_h[i],j,fb_r[i], tid);
+		}
+		else
+		{
+			while (ok[make_pair(j,fb_r[i])].count(fb_l[i])>0)
+				j=rand_max(entity_num);
+			train_kb(fb_h[i],fb_l[i],fb_r[i],j,fb_l[i],fb_r[i], tid);
+		}
+                norm(relation_tmp[fb_r[i]]);
+                norm(entity_tmp[fb_h[i]]);
+                norm(entity_tmp[fb_l[i]]);
+                norm(entity_tmp[j]);
+	}
+    }
+    
     void bfgs()
     {
-        res=0;
-        int nbatches=100;
-        int nepoch = 1000;
-        int batchsize = fb_h.size()/nbatches;
-            for (int epoch=0; epoch<nepoch; epoch++)
+        nbatches=100;
+        nepoch = 1000;
+        batchsize = fb_h.size()/nbatches/THREADS_NUM;
+       for (int epoch=0; epoch<nepoch; epoch++)
+       {
+       	    res=0;
+            time_t epoch_s, epoch_e;
+            epoch_s = time(NULL);
+            for (int batch = 0; batch<nbatches; batch++)
             {
-
-            	res=0;
-		time_t epoch_s, epoch_e;
-		epoch_s = time(NULL);
-             	for (int batch = 0; batch<nbatches; batch++)
-             	{
-			time_t start = time(NULL), stop;
-			int tmp_res = res;
-             		relation_tmp=relation_vec;
-            		entity_tmp = entity_vec;
-             		for (int k=0; k<batchsize; k++)
-             		{
-						int i=rand_max(fb_h.size());
-						int j=rand_max(entity_num);
-						double pr = 1000*right_num[fb_r[i]]/(right_num[fb_r[i]]+left_num[fb_r[i]]);
-						if (method ==0)
-			                            pr = 500;
-						if (rand()%1000<pr)
-						{
-							while (ok[make_pair(fb_h[i],fb_r[i])].count(j)>0)
-								j=rand_max(entity_num);
-							train_kb(fb_h[i],fb_l[i],fb_r[i],fb_h[i],j,fb_r[i]);
-						}
-						else
-						{
-							while (ok[make_pair(j,fb_r[i])].count(fb_l[i])>0)
-								j=rand_max(entity_num);
-							train_kb(fb_h[i],fb_l[i],fb_r[i],j,fb_l[i],fb_r[i]);
-						}
-                		norm(relation_tmp[fb_r[i]]);
-                		norm(entity_tmp[fb_h[i]]);
-                		norm(entity_tmp[fb_l[i]]);
-                		norm(entity_tmp[j]);
-             		
-			}
-			stop = time(NULL);
-			cout << "batch: " << batch << " res: " << res - tmp_res << " time" << (double)(stop-start) << endl;
-		        relation_vec = relation_tmp;
-		        entity_vec = entity_tmp;
-             	}
-		epoch_e = time(NULL);
-                cout<<"epoch:"<<epoch<<' '<<res<<(double)(epoch_e-epoch_s)<<endl;
-                FILE* f2 = fopen(("relation2vec."+version).c_str(),"w");
-                FILE* f3 = fopen(("entity2vec."+version).c_str(),"w");
-		if(f2 ==  NULL || f3 == NULL)
-		   cout << "file open failed" << endl;
-                for (int i=0; i<relation_num; i++)
-                {
-                    for (int ii=0; ii<n; ii++)
-                        fprintf(f2,"%.6lf\t",relation_vec[i][ii]);
-                    fprintf(f2,"\n");
-                }
-                for (int i=0; i<entity_num; i++)
-                {
-                    for (int ii=0; ii<n; ii++)
-                        fprintf(f3,"%.6lf\t",entity_vec[i][ii]);
-                    fprintf(f3,"\n");
-                }
-                fclose(f2);
-                fclose(f3);
+                time_t start = time(NULL), stop;
+            	int tmp_res = res;
+         	relation_tmp=relation_vec;
+        	entity_tmp = entity_vec;
+            	pthread_t threads[THREADS_NUM];
+            	for(int k=0; k<THREADS_NUM; k++)
+            		res_thread[k] = 0;
+            	for(int k = 0; k<THREADS_NUM; k++)
+            		pthread_create(&threads[k], NULL, rand_sel, (void *)k);
+            	for(int k = 0; k<THREADS_NUM; k++)
+            		pthread_join(threads[k], NULL);
+            	for(int k=0; k<THREADS_NUM; k++)
+            		res += res_thread[k];
+            	stop = time(NULL);
+            	cout << "batch: " << batch << " res: " << res - tmp_res << " time" << (double)(stop-start) << endl;
+                relation_vec = relation_tmp;
+                entity_vec = entity_tmp;
             }
+            epoch_e = time(NULL);
+            cout<<"epoch:"<<epoch<<' '<<res<<(double)(epoch_e-epoch_s)<<endl;
+            FILE* f2 = fopen(("relation2vec."+version).c_str(),"w");
+            FILE* f3 = fopen(("entity2vec."+version).c_str(),"w");
+            if(f2 ==  NULL || f3 == NULL)
+               cout << "file open failed" << endl;
+            for (int i=0; i<relation_num; i++)
+            {
+                for (int ii=0; ii<n; ii++)
+                    fprintf(f2,"%.6lf\t",relation_vec[i][ii]);
+                fprintf(f2,"\n");
+            }
+            for (int i=0; i<entity_num; i++)
+            {
+                for (int ii=0; ii<n; ii++)
+                    fprintf(f3,"%.6lf\t",entity_vec[i][ii]);
+                fprintf(f3,"\n");
+            }
+            fclose(f2);
+            fclose(f3);
+        }
     }
-    double res1;
-    vector<double> get_neighbors_attention_vec(int e, int rel)
-    {
-	vector<double> attention_vec(n);
-	vector<double> weight(entity_neighbors[make_pair(e, rel)].size());
-	double sum = 0;
-    	for (int i = 0; i<entity_neighbors[make_pair(e, rel)].size(); i++)
-	{
-		sum += exp(get_sum(e, entity_neighbors[make_pair(e, rel)][i], rel));
-		weight.push_back(exp(get_sum(e, entity_neighbors[make_pair(e, rel)][i], rel)));
-	}
-	for (int ii=0; ii<n; ii++)
-	{
-		attention_vec[ii] = 0.0;
-		for (int i=0; i<entity_neighbors[make_pair(e, rel)].size(); i++)
-			attention_vec[ii] += weight[i]/sum*entity_vec[entity_neighbors[make_pair(e, rel)][i]][ii];
-	}
-	return attention_vec;
-    }
-    double get_sum(int e1, int e2, int rel)
+   
+    double get_sum(int e1, int e2, int rel, int tid)
     {
 	double sum=0;
 	if (L1_flag)
@@ -235,11 +239,30 @@ private:
 			sum+=sqr(entity_vec[e2][ii]-entity_vec[e1][ii]-relation_vec[rel][ii]);
 	return sum;
     }
-	
-    double calc_sum(int e1,int e2,int rel)
+
+    vector<double> get_neighbors_attention_vec(int e, int rel, int tid)
     {
-	vector<double> neighbors_e1 = get_neighbors_attention_vec(e1, rel);
-	vector<double> neighbors_e2 = get_neighbors_attention_vec(e2, rel);
+	vector<double> attention_vec(n);
+	vector<double> weight(entity_neighbors[make_pair(e, rel)].size());
+	double sum = 0;
+    	for (int i = 0; i<entity_neighbors[make_pair(e, rel)].size(); i++)
+	{
+		sum += exp(get_sum(e, entity_neighbors[make_pair(e, rel)][i], rel, tid));
+		weight.push_back(exp(get_sum(e, entity_neighbors[make_pair(e, rel)][i], rel, tid)));
+	}
+	for (int ii=0; ii<n; ii++)
+	{
+		attention_vec[ii] = 0.0;
+		for (int i=0; i<entity_neighbors[make_pair(e, rel)].size(); i++)
+			attention_vec[ii] += weight[i]/sum*entity_vec[entity_neighbors[make_pair(e, rel)][i]][ii];
+	}
+	return attention_vec;
+    }
+	
+    double calc_sum(int e1,int e2,int rel, int tid)
+    {
+	vector<double> neighbors_e1 = get_neighbors_attention_vec(e1, rel, tid);
+	vector<double> neighbors_e2 = get_neighbors_attention_vec(e2, rel, tid);
         double sum=0;
         if (L1_flag)
         	for (int ii=0; ii<n; ii++)
@@ -249,12 +272,12 @@ private:
             	sum+=sqr(entity_vec[e2][ii]-entity_vec[e1][ii]-relation_vec[rel][ii]) + sqr(neighbors_e2[ii]-neighbors_e1[ii]-relation_vec[rel][ii] + con_alpha*(neighbors_e2[ii]-entity_vec[e2][ii] + neighbors_e1[ii]-entity_vec[e1][ii]));
         return sum;
     }
-    void gradient(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b)
+    void gradient(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b, int tid)
     {
-	vector<double> neighbors_e1_a = get_neighbors_attention_vec(e1_a, rel_a);
-	vector<double> neighbors_e2_a = get_neighbors_attention_vec(e2_a, rel_a);
-        vector<double> neighbors_e1_b = get_neighbors_attention_vec(e1_b, rel_b);
-        vector<double> neighbors_e2_b = get_neighbors_attention_vec(e2_b, rel_b);
+	vector<double> neighbors_e1_a = get_neighbors_attention_vec(e1_a, rel_a, tid);
+	vector<double> neighbors_e2_a = get_neighbors_attention_vec(e2_a, rel_a, tid);
+        vector<double> neighbors_e1_b = get_neighbors_attention_vec(e1_b, rel_b, tid);
+        vector<double> neighbors_e2_b = get_neighbors_attention_vec(e2_b, rel_b, tid);
         for (int ii=0; ii<n; ii++)
         {
 
@@ -278,19 +301,19 @@ private:
             entity_tmp[e2_b][ii]+=rate*x;
         }
     }
-    void train_kb(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b)
+    void train_kb(int e1_a,int e2_a,int rel_a,int e1_b,int e2_b,int rel_b, int tid)
     {
-        double sum1 = calc_sum(e1_a,e2_a,rel_a);
-        double sum2 = calc_sum(e1_b,e2_b,rel_b);
+        double sum1 = calc_sum(e1_a,e2_a,rel_a, tid);
+        double sum2 = calc_sum(e1_b,e2_b,rel_b, tid);
         if (sum1+margin>sum2)
         {
-        	res+=margin+sum1-sum2;
-        	gradient( e1_a, e2_a, rel_a, e1_b, e2_b, rel_b);
+        	res_thread[tid]+=margin+sum1-sum2;
+        	gradient( e1_a, e2_a, rel_a, e1_b, e2_b, rel_b, tid);
         }
     }
-};
+//};
 
-Train train;
+//Train train;
 void prepare()
 {
     FILE* f1 = fopen((data_path+"entity2id.txt").c_str(),"r");
@@ -335,7 +358,7 @@ void prepare()
         }
         left_entity[relation2id[s3]][entity2id[s1]]++;
         right_entity[relation2id[s3]][entity2id[s2]]++;
-        train.add(entity2id[s1],entity2id[s2],relation2id[s3]);
+        add(entity2id[s1],entity2id[s2],relation2id[s3]);
     }
     for (int i=0; i<relation_num; i++)
     {
@@ -400,5 +423,6 @@ int main(int argc,char**argv)
     cout<<"method = "<<version<<endl;
     cout<<"con_alpha = "<<con_alpha<<endl;
     prepare();
-    train.run(n,rate,margin,method, con_alpha);
+  //  train.run(n,rate,margin,method, con_alpha);
+    run(n, rate, margin, method, con_alpha);	
 }
